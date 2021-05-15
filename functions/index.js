0906c7b6d30153afd5b2725b7edf7253bf264555
Rokas <rookas.rudzenskas@gmail.com>
2021-05-15 14:48:28 +0300

    Rokas

--- functions/index.js
+++ functions/index.js
@@ -4,5 +4,15 @@ const cors = require("cors");
 const stripe = require("stripe")('sk_test_51IrKTaKakUNLk7QM9PVGr2AhY6MBd2Y3DyC8y19X1cekSlibzceH92qup2RQ29mdN3I5rxJiWJmgzEr0JecaYrgQ00s757rdW3');
 
 
+// for API setup
+
+
+// App config
+
+// middlewares
+
+// API routes
+
+// Listen command
 
 
