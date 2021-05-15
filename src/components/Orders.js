3b2051729d7e91daa58265d5a8aa20ffc72edea3
Rokas <rookas.rudzenskas@gmail.com>
2021-05-15 15:58:03 +0300

    Rokas

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,6 +1,24 @@
-import React from 'react';
-import "./styles/Orders.css"
+import React, {useEffect, useState} from 'react';
+import "./styles/Orders.css";
+import db from "../firebase";
+import {useStateValue} from "../StateProvider";
+
 const Orders = () => {
+
+    const [{basket, user}, dispatch] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+
+    useEffect(() => {
+    //        will run once, then the orders run
+        db.collection("users").doc(user?.uid).collection("orders").orderBy('created', 'desc').onSnapshot(snapshot => {
+           setOrders(snapshot.docs.map(doc => ({
+               id: doc.id,
+               data: doc.data(),
+           })))
+        });
+    }, []);
+
     return (
         <div className="orders">
             <h1>Your orders</h1>
