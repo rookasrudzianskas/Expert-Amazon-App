0f4d8149965de9cc7f606d99635a7f222d05e94c
Rokas <rookas.rudzenskas@gmail.com>
2021-05-15 21:29:38 +0300

    Rokas

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./styles/Order.css";
 import moment from "moment";
 import CheckoutProduct from "./CheckoutProduct";
+import CurrencyFormat from "react-currency-format";
 
 const Order = ({ order }) => {
 
@@ -27,6 +28,21 @@ const Order = ({ order }) => {
                     hideButton
                 />
             ))}
+
+            <CurrencyFormat
+                renderText={(value) => (
+                    <>
+                        <h3 className="order__total">Order Total: {value}</h3>
+                    </>
+                )}
+
+                decimalScale={2}
+                {/* because it was converted to cents*/}
+                value={order.data.amount / 100}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+                />
 
         </div>
     );
