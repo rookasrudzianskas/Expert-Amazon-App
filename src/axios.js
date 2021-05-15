0e5d8c80308fd6e05675185f7b88b92721073543
Rokas <rookas.rudzenskas@gmail.com>
2021-05-15 14:01:10 +0300

    Rkoas

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// this creates axios request to the url
 const instance = axios.create({
     // THE API (cloud function) URL
     baseURL: 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api'
