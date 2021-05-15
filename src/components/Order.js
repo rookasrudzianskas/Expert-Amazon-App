98e4e03371e8774d4630f39c45c8730d297c18de
Rokas <rookas.rudzenskas@gmail.com>
2021-05-15 21:34:16 +0300

    Rokas

--- src/components/Order.js
+++ src/components/Order.js
@@ -31,18 +31,14 @@ const Order = ({ order }) => {
 
             <CurrencyFormat
                 renderText={(value) => (
-                    <>
-                        <h3 className="order__total">Order Total: {value}</h3>
-                    </>
+                    <h3 className="order__total">Order Total: {value}</h3>
                 )}
-
                 decimalScale={2}
-                {/* because it was converted to cents*/}
                 value={order.data.amount / 100}
                 displayType={"text"}
                 thousandSeparator={true}
                 prefix={"$"}
-                />
+            />
 
         </div>
     );
