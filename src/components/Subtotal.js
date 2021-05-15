4045944947d726c79a8e7bc6ad7d872870d68fbf
Rokas <rookas.rudzenskas@gmail.com>
2021-05-15 11:38:31 +0300

    Rokas:

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -3,10 +3,11 @@ import "./styles/Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import {useStateValue} from "../StateProvider";
 import {getBasketTotal} from "../reducer";
+import {useHistory} from "react-router-dom";
 
 
 const Subtotal = () => {
-
+    const history = useHistory();
     const [{basket}, dispatch] = useStateValue();
 
     return (
@@ -31,7 +32,7 @@ const Subtotal = () => {
                 thousandSeparator={true}
                 prefix={"$"}
             />
-            <button>Proceed to Checkout</button>
+            <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
         </div>
     );
 };
