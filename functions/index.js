67c2b9993d33fab46e1ba5f4ab41a944e8751b08
Rokas <rookas.rudzenskas@gmail.com>
2021-05-15 14:47:15 +0300

    Rokas

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51IrKTaKakUNLk7QM9PVGr2AhY6MBd2Y3DyC8y19X1cekSlibzceH92qup2RQ29mdN3I5rxJiWJmgzEr0JecaYrgQ00s757rdW3');
+
+
 
 
