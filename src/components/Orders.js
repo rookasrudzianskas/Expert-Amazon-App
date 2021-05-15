39a6f8a3f50a2aa9e7cd67407f75234b2ce9273b
Rokas <rookas.rudzenskas@gmail.com>
2021-05-15 16:11:39 +0300

    Rokas

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -2,14 +2,17 @@ import React, {useEffect, useState} from 'react';
 import "./styles/Orders.css";
 import db from "../firebase";
 import {useStateValue} from "../StateProvider";
+import Order from "./Order";
 
 const Orders = () => {
 
     const [{basket, user}, dispatch] = useStateValue();
     const [orders, setOrders] = useState([]);
-
+    console.log(orders);
+    //if we use outside variable we have to put it into the brackets in the useEffect dependencies
 
     useEffect(() => {
+        if(user) {
     //        will run once, then the orders run
     //     going to users, getting specific user, accessging that particulat users collection orders, we order by date, take snapshot,
     //    which is mapping throug all the orders, and setting all the data to the orders collection
@@ -20,11 +23,21 @@ const Orders = () => {
                data: doc.data(),
            })))
         });
-    }, []);
+        } else {
+            setOrders([]);
+        }
+    }, [user]);
 
     return (
         <div className="orders">
             <h1>Your orders</h1>
+
+            <div className="orders__order">
+                {/* we go per all the orders and output followign for each one of them*/}
+                {orders.map(order => {
+                    <Order order={order} />
+                })}
+            </div>
         </div>
     );
 };
