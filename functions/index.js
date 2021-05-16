bb314b6684e9ca8dadf19fe00ce00da14d430b57
Rokas <rookas.rudzenskas@gmail.com>
2021-05-16 15:52:38 +0300

    Rokas

--- functions/index.js
+++ functions/index.js
@@ -4,42 +4,39 @@ const cors = require("cors");
 const stripe = require("stripe")('sk_test_51IrKTaKakUNLk7QM9PVGr2AhY6MBd2Y3DyC8y19X1cekSlibzceH92qup2RQ29mdN3I5rxJiWJmgzEr0JecaYrgQ00s757rdW3');
 
 
-// for API setup
 
+// API
 
-// App config
+// - App config
 const app = express();
 
-// middlewares
-// security shit
+// - Middlewares
 app.use(cors({ origin: true }));
-//send and get data in json format
-app.use(express.json())
-// API routes
+app.use(express.json());
+
+// - API routes
 app.get("/", (request, response) => response.status(200).send("hello world"));
-app.post('/payments/create', async (request, response) => {
-    // this is amount in subunits
+
+app.post("/payments/create", async (request, response) => {
     const total = request.query.total;
 
-    console.log("Payment request received BOOM! >>>>>>>", total);
-    // we created the payment in here
+    console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: total, // we get the total
-        currency: "usd" // the currency is usd
+        amount: total, // subunits of the currency
+        currency: "usd",
     });
-    // 201 okay everything worked good
+
+    // OK - Created
     response.status(201).send({
-        // this is the code that we want to make the payment
         clientSecret: paymentIntent.client_secret,
+    });
+});
 
-    })
-})
-// Listen command
-
+// - Listen command
 exports.api = functions.https.onRequest(app);
 
-
-//example endpoint
-//http://localhost:5001/rookas-amzon-app/us-central1/api
+// Example endpoint
+// http://localhost:5001/challenge-4b2b2/us-central1/api
 
 
