9ab811d1438a8d4ca0ee7f260193ca1491ae55ae
Rokas <rookas.rudzenskas@gmail.com>
2021-05-16 15:56:17 +0300

    Rokas

--- src/axios.js
+++ src/axios.js
@@ -5,6 +5,7 @@ const instance = axios.create({
     // THE API (cloud function) URL
     baseURL: 'https://us-central1-rookas-amzon-app.cloudfunctions.net/api' // this is the api url, cloud function
     // "http://localhost:5001/challenge-4b2b2/us-central1/api",
+    // http://localhost:5001/rookas-amzon-app/us-central1/api the local one
 });
 
 export default instance;
